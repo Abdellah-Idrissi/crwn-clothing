2e8c3b86af7057a1af902e1865b66d6bab5d31a0
Abdellah-Idrissi <ellidrissiabdellah@gmail.com>
2023-06-02 19:06:04 +0100

    adding env vars for our firebase data

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app"
-import {GoogleAuthProvider, getAuth} from "firebase/auth"
+import { GoogleAuthProvider, getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCEaAoVqHIJIKINB7XwSKdN_liXkSGa9DU",
-  authDomain: "crwn-cloting-4c1dc.firebaseapp.com",
-  projectId: "crwn-cloting-4c1dc",
-  storageBucket: "crwn-cloting-4c1dc.appspot.com",
-  messagingSenderId: "715701449367",
-  appId: "1:715701449367:web:bb5af52b843572082eefd2"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
-
-
 
 
 const app = initializeApp(firebaseConfig)
